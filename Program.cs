59a5a032e7f6f0ac967dfa3ec2b563c73c8e681a
Arunesh sharma <73111126+as8175802986@users.noreply.github.com>
2021-11-11 20:34:31 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,8 +10,70 @@ namespace Snake_Ladder
     {
         static void Main(string[] args)
         {
+            
+        //Constants 
+        public const int NO_PLAY = 0;
+        public const int LADDER = 1;
+        public const int SNAKE = 2;
+        public const int FINAL = 100;
+
+        static void Main(string[] args)
+        {
             Console.WriteLine("**Welcome to the game of snake and ladder**");
             Console.WriteLine();
+            Console.WriteLine("Single Player mode: Starting position 0");  //as per UC1
+            Console.WriteLine("....Game Begins....");
+            Console.WriteLine();
+            
+            //Variables
+            int position = 0;
+            int count = 0;
+
+            Random die = new Random();     //creatting random object from random class
+            Random options = new Random();
+
+            while (position < FINAL)
+            {
+                int dice = die.Next(1, 7);       //simulating the die throw 
+                //Console.WriteLine("The number on this die roll is: " + dice);
+                int opt = options.Next(0, 3);    //simulating the options
+                count++;                         //counter to count the number of die thrown
+
+                //options use
+                if (opt == NO_PLAY)
+                {
+                    //Console.WriteLine("No play: Player in same position-- " + position);
+                }
+                else if (opt == LADDER)
+                {
+                    position = position + dice;
+                    if (position > 100)
+                    {
+                        //Console.WriteLine("Try Again, throw exceeded 100!");
+                        position = position - dice;
+                    }
+                    else
+                    {
+                       // Console.WriteLine("Ladder! new postion-- " + position);
+                    }
+
+                }
+                else
+                {
+                    position = position - dice;
+                    //Console.WriteLine("Oops,Snake! new position-- " + position);
+                }
+
+                if (position < 0)
+                {
+                    position = 0;
+                }
+
+                Console.WriteLine("The position after the {0} die roll is {1} " ,count,position);
+            }
+            Console.WriteLine();
+            Console.WriteLine("The number of times the die is thrown is: " + count);
+        }
         }
     }
 }
