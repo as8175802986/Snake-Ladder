7717755788329d81f84965615d95fb4ac7b39b7a
Arunesh sharma <73111126+as8175802986@users.noreply.github.com>
2021-11-11 20:30:52 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,10 +8,62 @@ namespace Snake_Ladder
 {
     class Program
     {
+       //Constants 
+        public const int NO_PLAY = 0;
+        public const int LADDER = 1;
+        public const int SNAKE = 2;
+        public const int FINAL = 100;
+
         static void Main(string[] args)
         {
             Console.WriteLine("**Welcome to the game of snake and ladder**");
             Console.WriteLine();
+            Console.WriteLine("Single Player mode: Starting position 0");  //as per UC1
+            Console.WriteLine("....Game Begins....");
+            
+            //Variables
+            int position = 0;
+
+            Random die = new Random();     //creatting random object from random class
+            Random options = new Random();
+
+            while (position < FINAL)
+            {
+                int dice = die.Next(1, 7);       //simulating the die throw 
+                Console.WriteLine("The number on this die roll is: " + dice);
+                int opt = options.Next(0, 3);    //simulating the options
+
+                //options use
+                if (opt == NO_PLAY)
+                {
+                    Console.WriteLine("No play: Player in same position-- " + position);
+                }
+                else if (opt == LADDER)
+                {
+                    position = position + dice;
+                    if (position > 100)
+                    {
+                        Console.WriteLine("Try Again, throw exceeded 100!");
+                        position = position - dice;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ladder! new postion-- " + position);
+                    }
+
+                }
+                else
+                {
+                    position = position - dice;
+                    Console.WriteLine("Oops,Snake! new position-- " + position);
+                }
+
+                if (position < 0)
+                {
+                    position = 0;
+                }
+            }
+            
         }
     }
 }
